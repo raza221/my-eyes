1f7ea9907b2a534635b2acdb021197f3d271fbb7
raza <rsyed9454@gmail.com>
2022-09-09 21:29:27 +0100

    add bg.jpg modify main.py

--- main.py
+++ main.py
@@ -1,45 +1,79 @@
 from gtts import gTTS
+import tkinter as tk
+from tkinter.filedialog import askopenfilename
 import os
-from PIL import Image
 import subprocess
 import cv2
 import pytesseract
 from playsound import playsound 
 
 os.chdir(r"C:\Users\raza\Documents\PythonProjects\my-eyes")
+
+def camera1():
+    cam = cv2.VideoCapture(0)
+    
+    cv2.namedWindow("Take a picture of the text.")
+    
+    img_counter = 0
+    
+    while True:
+        ret, frame = cam.read()
+        if not ret:
+            print("Failed to grab frame!")
+            break
+        cv2.imshow("Take a picture of the text.", frame)
+    
+        k = cv2.waitKey(1)
+        if k%256 == 27:
+            # ESC pressed
+            global picture
+            picture = "image.png"
+            break
+        elif k%256 == 32:
+            # SPACE pressed
+            img_name = "image.png"
+            cv2.imwrite(img_name, frame)
+            print("{} written!".format(img_name))
+            img_counter += 1
+    
+    cam.release()
+    
+    cv2.destroyAllWindows()
+
+def pickfile():
+    global picture
+    picture = askopenfilename()
+    
+root = tk.Tk()
+root.title("MyRetina - Your Virtual Eyes")
+H = 700 
+W = 800
+
+background_image=tk.PhotoImage(r"C:\Users\raza\Documents\PythonProjects\my-eyes\bg.jpg")
+background_label = tk.Label(root, image=background_image)
+background_label.place(x=0, y=0, relwidth=1, relheight=1)
+
+canvas = tk.Canvas(root, height=H, width=W)
+canvas.pack()
+
+frame = tk.Frame(root, bg="#80c1ff")
+frame.place(relheight=1, relwidth=1)
+
+button = tk.Button(frame, text ="Take Photo", bg = "gray", fg="white", command=camera1)
+button.pack(expand=True,)
+
+button = tk.Button(frame, text ="Choose from Photo Library", bg = "gray", fg="white", command=pickfile)
+button.pack(expand=True,)
+
+
+root.mainloop()
+
+
 pytesseract.pytesseract.tesseract_cmd = (r'C:\Program Files\Tesseract-OCR\tesseract.exe')
 
-cam = cv2.VideoCapture(0)
- 
-cv2.namedWindow("Take a picture of the text.")
- 
-img_counter = 0
- 
-while True:
-    ret, frame = cam.read()
-    if not ret:
-        print("failed to grab frame")
-        break
-    cv2.imshow("Take a picture of the text.", frame)
- 
-    k = cv2.waitKey(1)
-    if k%256 == 27:
-        # ESC pressed
-        print("Escape hit, closing...")
-        break
-    elif k%256 == 32:
-        # SPACE pressed
-        img_name = "image.png"
-        cv2.imwrite(img_name, frame)
-        print("{} written!".format(img_name))
-        img_counter += 1
- 
-cam.release()
- 
-cv2.destroyAllWindows()
 
 # Grayscale, Gaussian blur, Otsu's threshold
-image = cv2.imread(r"C:\Users\raza\Documents\PythonProjects\my-eyes\roadsigns.png")
+image = cv2.imread(picture)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blur = cv2.GaussianBlur(gray, (3,3), 0)
 thresh = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
@@ -66,4 +100,4 @@ output = gTTS(text=myText, lang=language, slow=False, tld=accent)
 output.save("output.mp3")
 fh.close()
 
-playsound(r'C:\Users\raza\Documents\PythonProjects\my-eyes\output.mp3')
+playsound(r'output.mp3')
